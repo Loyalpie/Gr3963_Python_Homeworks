05682d812ef60ff58eb14a57d6fead71920d432c
Alex Slepov <alekseyslepov2003@gmail.com>
2023-12-29 17:55:19 +0300

    Sem7_classwork

---
+++ Sem07_classwork/task47.py
@@ -0,0 +1,5 @@
+transformation = lambda x: x
+values = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29] # или любой другой список
+transormed_values = list(map(transformation, values))
+
+print(transormed_values)
